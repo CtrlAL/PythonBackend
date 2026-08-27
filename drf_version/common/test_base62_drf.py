31cd2e09ee28775757e2af9897cde2553a387272
Саша Ларцев <sasha.lartsev@gmail.com>
2026-08-27 20:48:31 +0400

    feat(drf): add base62 util with tests

---
+++ drf_version/common/test_base62_drf.py
@@ -0,0 +1,9 @@
+from base62 import encode, decode
+
+def test_roundtrip():
+    for n in [0, 1, 42, 123456789, 2**40, 2**62]:
+        assert decode(encode(n)) == n
+
+def test_known():
+    assert encode(10) == "a"
+    assert encode(61) == "Z"
